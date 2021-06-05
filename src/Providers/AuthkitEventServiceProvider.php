e807eaffc555c51ed0c98feb61b9846fe54b0620
PashaZ <pasha.zahari@gmail.com>
2021-06-05 23:28:01 +0800

    Update AuthkitEventServiceProvider.php

--- src/Providers/AuthkitEventServiceProvider.php
+++ src/Providers/AuthkitEventServiceProvider.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace dhtech\Auth\Providers;
+namespace Dhtech\Auth\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
